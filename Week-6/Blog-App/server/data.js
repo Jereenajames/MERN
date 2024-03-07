f54ff4085e12ba3dd06b97512a6573debfdbc236
Jereena James <jereenajames1997@gmail.com>
2024-03-07 18:09:30 +0530

    mongoose

--- Week-6/Blog-App/server/data.js
+++ Week-6/Blog-App/server/data.js
@@ -1,6 +1,6 @@
 const users =[
     {id : 102, Name :'Jerry', email : "Jerry@gmail.com", Password : "1234"},
-    {id : 102, Name :'Jerrina', email : "Jerri@gmail.com", Password : "123"},
+    {id : 102, Name :'Jerina', email : "Jerri@gmail.com", Password : "123"},
 
 ]
 export default users;
---
+++ Week-6/Blog-App/server/models/userModel.js
@@ -0,0 +1,14 @@
+import mongoose from "moongoose"
+
+const userSchema =  new mongoose.Schema({
+    username :{
+        type : String ,
+        require : true,
+        trim: true,
+        min
+    },
+    email : {
+        type :String,
+        require : true,
+    }
+})
