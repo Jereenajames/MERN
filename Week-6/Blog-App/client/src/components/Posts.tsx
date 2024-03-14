59fb4a0020550678f51f3fc9db30a240381bb5a1
Jereena James <jereenajames1997@gmail.com>
2024-03-14 17:54:53 +0530

    Blog-App

--- Week-6/Blog-App/client/src/components/Posts.tsx
+++ Week-6/Blog-App/client/src/components/Posts.tsx
@@ -1,6 +1,16 @@
+import axios from "axios";
+import { useEffect,useState } from "react";
+
+
 const Posts = () =>{
+    useEffect(() =>{
+        axios
+        .get("http://localhost:5000/api/v1/posts")
+        .then((res) => setPosts(res.data.data))
+        .catch((error) =>alert(error.response.data.message))
+    })
     return(
-        
+
         <div>Posts</div>
     )
 }
--- Week-6/Blog-App/client/src/components/Signup.tsx
+++ Week-6/Blog-App/client/src/components/Signup.tsx
@@ -1,5 +1,27 @@
-const Signup =() =>{
+import Form from 'react-bootstrap/Form';
 
-    return <div>Signup</div>
+const Signup =() =>{
+    return <div>
+        <h2 className="display-4 text-center">Signup</h2>
+        <Form>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Form.Label>Username</Form.Label>
+        <Form.Control type="text" placeholder="Enter the User Name" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter the Email " />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="email" placeholder="Enter the Password" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Form.Label>Confrim Password</Form.Label>
+        <Form.Control type="email" placeholder="Enter the Password" />
+      </Form.Group>
+   
+    </Form>
+    </div>
 }
 export default Signup;
