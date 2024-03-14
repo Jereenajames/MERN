696271e06e86109d71b37cbe1bce477e62a55cd1
Jereena James <jereenajames1997@gmail.com>
2024-03-14 18:38:30 +0530

    Blog-App

--- Week-6/Blog-App/client/src/components/Signup.tsx
+++ Week-6/Blog-App/client/src/components/Signup.tsx
@@ -1,26 +1,38 @@
 import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
+
 
 const Signup =() =>{
+const [user, setUser] = useState({
+    username : "",
+    email : "",
+    password : "",
+    confirmPassword : ""
+});
+
     return <div>
         <h2 className="display-4 text-center">Signup</h2>
-        <Form>
+        <Form className=' me-5'>
       <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
         <Form.Label>Username</Form.Label>
-        <Form.Control type="text" placeholder="Enter the User Name" />
+        <Form.Control type="text" placeholder="Enter the User Name" name='username' value={user.}/>
       </Form.Group>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput2">
         <Form.Label>Email address</Form.Label>
         <Form.Control type="email" placeholder="Enter the Email " />
       </Form.Group>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput3">
         <Form.Label>Password</Form.Label>
         <Form.Control type="email" placeholder="Enter the Password" />
       </Form.Group>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput4">
         <Form.Label>Confrim Password</Form.Label>
-        <Form.Control type="email" placeholder="Enter the Password" />
+        <Form.Control type="email" placeholder="Enter the Password Again" />
       </Form.Group>
    
+      <Button variant="primary">Register</Button>{' '}
+      <Button variant="secondary">Cancle</Button>{' '}
     </Form>
     </div>
 }
